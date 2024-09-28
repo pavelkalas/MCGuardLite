e497c2ed91166a33ed4391fabab922563167ee27
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-28 17:39:27 +0200

    Added function onEnable, onDisable and plugin.yml file

--- src/cz/pavelkalas/core/PluginLoader.java
+++ src/cz/pavelkalas/core/PluginLoader.java
@@ -3,4 +3,18 @@ package cz.pavelkalas.core;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginLoader extends JavaPlugin {
+
+	/**
+	 * On plugin load.
+	 */
+	@Override
+	public void onEnable() {
+	}
+
+	/**
+	 * On plugin unload (stopping server, reloading, etc..)
+	 */
+	@Override
+	public void onDisable() {
+	}
 }
