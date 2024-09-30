9625a75bf329430a5140798a4a2299fc849906c3
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-30 21:32:53 +0200

    Added kick function with message

--- src/cz/pavelkalas/utils/PlayerUtils.java
+++ src/cz/pavelkalas/utils/PlayerUtils.java
@@ -7,11 +7,35 @@ public class PlayerUtils {
 	/**
 	 * Ban player's nickname and IP too.
 	 * 
-	 * @param player   Player name
-	 * @param message  Player IP address
+	 * @param player     Player instance
+	 * @param message    Player IP address
 	 */
 	public static void banPlayer(Player player, String message) {
-		CommandsSender.sendCommands(new String[] { "ban " + player.getName() + " " + message,
-				"ban-ip " + player.getAddress().getAddress().toString().substring(1) + " " + message });
+		if (message != null && player != null) {
+			CommandsSender.sendCommands(new String[] { "ban " + player.getName() + " " + message,
+					"ban-ip " + player.getAddress().getAddress().toString().substring(1) + " " + message });
+		}
+	}
+
+	/**
+	 * Kills the player.
+	 * 
+	 * @param player    Player instance
+	 */
+	public static void killPlayer(Player player) {
+		if (player != null && !player.isDead()) {
+			player.damage(Double.MAX_VALUE);
+		}
+	}
+	
+	/**
+	 * Kicks the player.
+	 * 
+	 * @param player   Player instance
+	 */
+	public static void kickPlayer(Player player, String message) {
+		if (player != null && !player.isDead() && message != null) {
+			CommandsSender.sendCommand("kick " + player.getName() + " " + message);
+		}
 	}
 }
