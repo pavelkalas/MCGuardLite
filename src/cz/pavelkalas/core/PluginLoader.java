50d4acd71702b87e141252ca5fc4fc88d00c2c58
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-30 20:30:48 +0200

    Added new listener for block interactions.

--- src/cz/pavelkalas/core/PluginLoader.java
+++ src/cz/pavelkalas/core/PluginLoader.java
@@ -3,7 +3,8 @@ package cz.pavelkalas.core;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import cz.pavelkalas.listeners.ConnectionListener;
-import cz.pavelkalas.listeners.PlayerListener;
+import cz.pavelkalas.listeners.player.BlockInteractionListener;
+import cz.pavelkalas.listeners.player.MovementListener;
 import cz.pavelkalas.utils.ConfigurationManager;
 
 public class PluginLoader extends JavaPlugin {
@@ -23,7 +24,8 @@ public class PluginLoader extends JavaPlugin {
 		
 		if (configManager.isLoaded()) {
 			getServer().getPluginManager().registerEvents(new ConnectionListener(configManager), this);
-			getServer().getPluginManager().registerEvents(new PlayerListener(configManager), this);
+			getServer().getPluginManager().registerEvents(new MovementListener(configManager), this);
+			getServer().getPluginManager().registerEvents(new BlockInteractionListener(configManager), this);
 		}
 	}
 
